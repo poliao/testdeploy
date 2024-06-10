79ed6af14162edd138a4b892bc2fa958e9670a35
Panu Somleamthong <sphanu9400@gmail.com>
2024-06-10 14:51:53 +0700

    commit

--- src/main/java/com/example/newweb/controller/PersonController.java
+++ src/main/java/com/example/newweb/controller/PersonController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
-@CrossOrigin(origins = "https://prismatic-snickerdoodle-7aba81.netlify.app")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/persons")
 public class PersonController {
